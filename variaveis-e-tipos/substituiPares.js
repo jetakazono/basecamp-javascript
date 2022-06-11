c0d84bdbf5f034e5c7985090a97d5ff4ca7c2413
jessie <jessica.takazono@gmail.com>
2022-06-12 00:53:29 +0100

    Adicionando minha solucao para atividade2 - SubstituiPares

--- variaveis-e-tipos/substituiPares.js
+++ variaveis-e-tipos/substituiPares.js
@@ -16,3 +16,26 @@ function substituiNumerosPares(array) {
 }
 
 substituiNumerosPares([2, 0, 3, 5]);
+
+/* My solution */
+function substituiPares(array){
+    if (!array.length) return console.log(-1);
+
+    for (let i = 0; i < array.length; i++) {
+        if(array[i]% 2 === 0){
+            array[i] = 0;
+        }else{
+            array[i] = array[i];
+        }
+    }
+	return console.log(array);
+}
+substituiPares([1, 3, 4, 6, 80, 33, 23, 90]);
+//  Exemplo:
+// Input -> [1, 3, 4, 6, 80, 33, 23, 90]
+
+// Output -> [1, 3, 0, 0, 0, 33, 23, 0]
+
+// Input -> []
+
+// Output -> -1
